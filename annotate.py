bbd95530fbc3c44faf6e1d9f6285abc49da0f9c8
Chris Wilding <hello@chriswilding.co.uk>
2020-01-04 21:48:18 +0000

    Add cid and nin

--- annotate.py
+++ annotate.py
@@ -3,12 +3,26 @@
 import base64
 import json
 import os
+import time
 import uuid
+
+DECK_ID = 1578170379751
 
 def get_anki_guid():
     u = uuid.uuid4()
     encoded = base64.encodebytes(u.bytes)
     return encoded[:10].decode("utf-8")
+
+def get_epoch_ms():
+    return int(time.time() * 1000)
+
+def annotate(entry):
+    if not 'guid' in entry:
+        entry['guid'] = get_anki_guid()
+    if not 'cid' in entry:
+        entry['cid'] = get_epoch_ms()
+    if not 'nid' in entry:
+        entry['nid'] = get_epoch_ms()
 
 def get_lesson_dirs():
     entries = os.listdir(os.path.join(os.getcwd(), 'lessons'))
@@ -23,8 +37,7 @@ for d in dirs:
     with open(path) as f:
         data = json.load(f)
         for entry in data:
-            if not 'guid' in entry:
-                entry['guid'] = get_anki_guid()
+            annotate(entry)
 
     with open(path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True)
