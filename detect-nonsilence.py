26642f1feba948e21d5bd7e02792f37954ffa64a
Chris Wilding <hello@chriswilding.co.uk>
2020-03-28 19:16:25 +0000

    Add audio for lessons 20

--- detect-nonsilence.py
+++ detect-nonsilence.py
@@ -5,8 +5,7 @@ import os
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
 
-MP3 = 'Lesson 04-3.mp3'
-# MP3 = '第14课-3.mp3'
+MP3 = '第20课-3.mp3'
 
 path = os.path.join(os.getcwd(), 'media', MP3)
 audio = AudioSegment.from_mp3(path)
